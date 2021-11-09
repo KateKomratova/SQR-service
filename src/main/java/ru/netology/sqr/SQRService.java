d7ec6990dcd038475da62a5b514b9da94b513646
KateKomratova <91473706+KateKomratova@users.noreply.github.com>
2021-11-09 14:54:07 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,7 +1,7 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int CalculateNumberSquares (int upperBorder, int bottomBorder) {
+    public int calculateNumberSquares (int upperBorder, int bottomBorder) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= upperBorder && i * i <= bottomBorder) {
